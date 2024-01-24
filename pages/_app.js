348296cb2665c4be6f7ec3484124766f4c9ee07e
Niko Cremer <n.ko@posteo.de>
2024-01-24 11:08:33 +0100

    add SWRConfig and mutate

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,19 @@
 import GlobalStyle from "../styles";
+import { SWRConfig } from "swr";
 
 export default function App({ Component, pageProps }) {
+  const { mutate } = useSWRConfig();
+  mutate(key, data, options);
   return (
     <>
-      <GlobalStyle />
-      <Component {...pageProps} />
+      <SWRConfig
+        value={{
+          refreshInterval: 1000,
+        }}
+      >
+        <GlobalStyle />
+        <Component {...pageProps} />
+      </SWRConfig>
     </>
   );
 }
